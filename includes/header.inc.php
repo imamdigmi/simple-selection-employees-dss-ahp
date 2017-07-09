f0f821fba7bdafb756d6177319968e475616f575
imamdigmi <imam.digmi@gmail.com>
2017-07-09 21:51:48 +0700

    Add: nilai awal

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -52,6 +52,7 @@ $db = $config->getConnection();
               <?php if ($_SESSION["role"] == "atasan"): ?>
                   <li role="presentation"><a href="data-kriteria.php">Kriteria</a></li>
                   <li role="presentation"><a href="nilai.php">Nilai</a></li>
+                  <li role="presentation"><a href="nilai-awal.php">Nilai Awal</a></li>
               <?php endif; ?>
 
               <?php if ($_SESSION["role"] == "atasan"): ?>
